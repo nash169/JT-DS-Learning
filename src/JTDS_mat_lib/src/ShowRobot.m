6d0d530d4f5a593f5557cc3e389d052a50434081
Bernardo <bernardo.fichera@gmail.com>
2018-03-03 10:19:40 +0100

    New "ShowRobot" function
    The funciton can visualize robots with multiple arms.

--- src/JTDS_mat_lib/src/ShowRobot.m
+++ src/JTDS_mat_lib/src/ShowRobot.m
@@ -1,6 +1,32 @@
-function fig = ShowRobot(robot,robot_fig)
+function fig = ShowRobot(robot, fig)
 %SHOWROBOT Summary of this function goes here
 %   Detailed explanation goes here
-fig = figure (100);
+
+plot_options = {'noshadow','nojaxes', 'nowrist', 'noname','jointdiam', 2,...
+    'basewidth', 1, 'linkcolor',0.7*[1,1,1], 'ortho','noshading',...
+    'notiles','jointcolor',0.6*[1,1,1]};
+
+if nargin == 1
+    fig = figure (100);
+else
+    axes(fig);
 end
 
+fields = fieldnames(robot); 
+
+robot_handle = robot(1).(fields{1}).robot;
+robot_handle.plotopt = plot_options;
+robot_handle.plot(zeros(1, robot_handle.n));
+
+hold on
+view([0 90])
+
+if length(fields) > 1
+    for i = 2:length(fields)
+        robot_handle = robot(1).(fields{i}).robot;
+        robot_handle.plotopt = plot_options;
+        robot_handle.plot(zeros(1, robot_handle.n));
+    end
+end
+
+end
